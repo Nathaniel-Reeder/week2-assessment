8b4bef5ed548f10400c9ba7fc7768d734c21c4bd
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-02-24 13:16:51 -0700

    finished orders.js

--- orders.js
+++ orders.js
@@ -30,7 +30,18 @@
 */
 
 //CODE HERE
-
+class Ticket{
+    constructor(items, orderTime, customerID){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerID = customerID;
+        this.status = 'queued'
+    }
+    updateStatus(newStatus) {
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerID} is now ${this.status}.`)
+    }
+}
 
 
 /*
@@ -46,7 +57,8 @@
 
 //CODE HERE
 
-
+const firstTicket = new Ticket (['pizza', 'bread', 'soda'], '7:03 PM', 575)
+console.log(firstTicket)
 /*
     Call the `updateStatus` method on
     `firstTicket` passing in the string
@@ -54,3 +66,6 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus('cooking')
+console.log(firstTicket)
